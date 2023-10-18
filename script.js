dc3eab990a7b9e6ae8ec1084e4b50aafb8488815
Erdal Tok <erdaltok@hotmail.de>
2023-10-18 18:01:11 +0200

    preliminary final version

--- script.js
+++ script.js
@@ -16,11 +16,12 @@ const colors = {
   ice: "#e0f5ff ",
 };
 
+// holt den Hauptcontainer für alle Pokémon-Boxen
 const pokeContainer = document.getElementById("poke-container");
 
-let allPokemonBoxes = [];
+let allPokemonBoxes = []; // hier ganz normal eckige Klammern, da ich durch ein Array durch iterieren möchte.
 let currentPokemonId = 1;
-let pokemonDataCache = {};
+let pokemonDataCache = {}; // hier geschweifte Klammern verwenden müssen, da ein String erstellt werden musste
 let pokemonCache = {};
 
 const pokeLimit = 30;
@@ -37,14 +38,14 @@ async function getPokemon(id) {
   let url = `https://pokeapi.co/api/v2/pokemon/${id}`;
   let response = await fetch(url);
   let data = await response.json();
-  pokemonCache[id] = data; // Daten im Cache speichern
+  pokemonCache[id] = data; // abgerufene Daten im Cache speichern
   createPokemonBox(data);
 }
 
-
+// Erstellt die Box für ein einzelnes Pokemon und fügt sie dem Hauptcontainer hinzu + generiert den HTML Code
 function createPokemonBox(pokemon) {
-  const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1); // Zieht den Namen aus dem ArrayApi und schreibt den ersten Buchstaben groß. Mit slice wird nur der erste Buchstabe (1) groß geschrieben und der Rest wieder klein
-  const id = pokemon.id.toString().padStart(3, "0"); // Erst in ein String verwandeln, dann die ID-Nummern 3-Stellig werden und die nicht besetzten Stellen mit '0' ausgefüllt werden. zB. 001 statt nur 1 oder 010 statt 10.
+  const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1); // Zieht den Namen aus der Array-API und schreibt den ersten Buchstaben groß. Mit slice wird nur der erste Buchstabe (1) groß geschrieben und der Rest wieder klein
+  const id = pokemon.id.toString().padStart(3, "0"); // Erst in ein String verwandeln, damit die ID-Nummern 3-stellig werden und die nicht besetzten Stellen mit '0' ausgefüllt werden. zB. 001 statt nur 1 oder 010 statt 10.
   const weight = pokemon.weight;
   const type = pokemon.types[0].type.name;
   const color = colors[type];
@@ -70,6 +71,7 @@ function createHtmlOfPokemon(id, name, weight, type) {
   `;
 }
 
+// Search-Funktion für das Input-feld: Sucht nach Pokemon
 function filterNames() {
   let pokeSearch = document.getElementById("poke-input").value.toLowerCase();
   pokeContainer.innerHTML = "";
@@ -83,11 +85,12 @@ function filterNames() {
     }
   });
 }
-
+// Löscht den Inhalt des Input-Feldes nach getätigter Suche
 function clearInput() {
   document.getElementById("poke-input").value = "";
 }
 
+// Holt die Gesamtdaten aus dem Cache und genriert inkl. HTML Template die BigView-Ansicht + try / catch Funktion, um zu prüfen, ob ein Fehler bzw. Error kommt.
 async function createBigViewPokemonBox(id) {
   try {
     let pokemon = pokemonCache[id]; // Daten aus dem Cache holen
@@ -117,22 +120,26 @@ function createBigViewPokemonHtml(id, name, paddedId, weight, type, height, expe
     <div class="center">
         <div class="poke-big-view-box">
             <div class="close-btn">
-                <img src="./img/close-btn.png" class="close-btn" onclick="closeBigView()"alt="">
+                <img src="./img/close-button.png" class="close-btn" onclick="closeBigView()"alt="">
             </div>
+
                 <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/${paddedId}.png" alt="" onerror="console.error('Fehler beim Laden des Bildes:', this.src);">
 
             <nav class="nav-btns">
-            <div><img src="./img/left-btn.png" onclick="leftButton()" id="leftButton"></div>
-            <div><img src="./img/right-btn.png" onclick="rightButton()" id="rightButton"></div>
+            <div><img src="./img/left-button.png" onclick="leftButton()" id="leftButton"></div>
+            <div><img src="./img/right-button.png" onclick="rightButton()" id="rightButton"></div>
             </nav>
+
             <h4 class="poke-name">${name}</h4>
             <p class="poke-id">#${paddedId}</p>
             <p class="poke-type">Type: ${type}</p>
+
             <div class="poke-big-view-box-info" id="poke-big-view-box-info-id">
                 <h2 onclick="openAbout()">About</h2>
                 <h2 onclick="openChart()">Stats</h2>
                 <h2 onclick="openAbilities()">Abilities</h2>
             </div>
+
             <div class="poke-big-view-box-info-content">
 
                 <div class="box-info-about hidden" id="box-info-about-id">
@@ -149,40 +156,40 @@ function createBigViewPokemonHtml(id, name, paddedId, weight, type, height, expe
                         <h4><b>Experience:</b></h4>
                         <h4>${experience}</h4>
                     </div>
-            </div>
+                </div>
 
                 <div class="box-info-stats-details hidden" id="box-info-stats-details-id">
-                        <canvas id="myChartId" width="306" height="152" style-"display: block; box-sizing: border-box; height: 137px; width: 306px;"> </canvas>
+                      <canvas id="myChartId" width="306" height="152" style-"display: block; box-sizing: border-box; height: 137px; width: 306px;"> </canvas>
                 </div>
 
                 <div class="box-info-ablility-details hidden" id="box-info-ablility-details-id">
                     <div class="abilities">
                         <h4>${abilities}</h4>
-                    </div>
-
                 </div>
             </div>
+          </div>
         </div>
-        </div>
+      </div>
   `;
  
 }
 
-
+// (1) Funktion nur zum Öffnen der BigView-Ansicht. Zur Übersichtlichkeit auf DREI Funktionen aufgeteilt (d.h. inkl. den beiden folgenden Funktionen "displayBigView(id)" und "setupPokemonStatsChart(id)")
  async function openBigView(id) {
    if (!pokemonCache[id]) {
      await getPokemon(id);
    }
 
+   // Zeigt die detaillierte Ansicht
    const bigViewContainer = document.getElementById("big-view-poke-container");
-   bigViewContainer.innerHTML = await createBigViewPokemonBox(id); // Stellen Sie sicher, dass das HTML korrekt eingefügt wird
+   bigViewContainer.innerHTML = await createBigViewPokemonBox(id); // Hier wird der HTML-Code eingefügt.
 
-   displayBigView(id); // Rufen Sie displayBigView NACH dem Einfügen des HTML auf
-   setupPokemonStatsChart(id);
+   displayBigView(id); // Ruft die displayBigView NACH dem einfügen des HTML auf. Reihenfolge war wichtig, weil erst der HTML-Code erst vollständig generiert werden musste, damit BigView auch angezeigt wird. + Zugriff auf big-view-poke-container in der nächsten Funktion, war erst möglch, nachdem die HTML bereits vollständig geladen worden ist.
+   pokemonStatsChart(id);
    updateNavigationButtons();
    openAbout();
  }
-
+// (2) Reihenfolge war wichtig, weil erst der HTML-Code erst vollständig generiert werden musste. Zugriff auf big-view-poke-container in der nächsten Funktion, war erst möglch, nachdem die HTML bereits vollständig geladen worden ist.
  function displayBigView(id) {
    const bigViewContainer = document.getElementById("big-view-poke-container");
 
@@ -199,12 +206,11 @@ function createBigViewPokemonHtml(id, name, paddedId, weight, type, height, expe
    bigViewContainer.style.display = "block";
    currentPokemonId = id;
 
-   setupCloseOnOutsideClick(); // Fügen Sie diese Zeile hinzu
+   CloseOnOutsideClick(); // Funktionsaufruf, um die Close-Funktion außerhalb von der Pokemon-Box in der BigView einzufügen
  }
 
-
-
- function setupPokemonStatsChart(id) {
+// (3) Funktion für die Stats in Chart.js. Hierzu separate Datei erstellt "pokemonChart.js" und diesen im HTML "<script src="pokemonChart.js"></script>" eingefügt.
+ function pokemonStatsChart(id) {
    const ctx = document.getElementById("myChartId").getContext("2d");
    const labels = [
      "hp",
@@ -221,36 +227,33 @@ function createBigViewPokemonHtml(id, name, paddedId, weight, type, height, expe
    createPokemonChart(ctx, labels, data, colors[type]);
  }
 
-
+// Zieht die Stats aus dem Parameter: Pokémon
  function getPokemonStats(pokemon) {
-   let stats = {};
+   let stats = {}; // hier geschweifte Klammern verwenden müssen, da ein String erstellt werden musste
    pokemon.stats.forEach((stat) => {
      stats[stat.stat.name] = stat.base_stat;
    });
    return stats;
  }
 
-
-
+// schließt die BigView-Ansicht
 function closeBigView() {
   const bigViewContainer = document.getElementById("big-view-poke-container");
-
   bigViewContainer.style.display = "none";
 }
 
-function setupCloseOnOutsideClick() {
+// Event-Listener hinzu, um die BigView-Ansicht zu schließen, wenn außerhalb der Box geklickt wird
+function CloseOnOutsideClick() {
   const bigViewContainer = document.getElementById("big-view-poke-container");
 
-  bigViewContainer.addEventListener("click", function (event) {
-    // Überprüfen, ob das Ziel des Klick-Events der bigViewContainer selbst ist
-    if (event.target === bigViewContainer) {
+  bigViewContainer.addEventListener("click", function (event) {   
+    if (event.target === bigViewContainer) { // schließt BigView-Ansicht , wenn auf innerhaln der DIV "big-view-poke-container" geklickt wird 
       closeBigView();
     }
   });
 }
 
-
-
+// nach links navigieren
 function leftButton() {
   if (currentPokemonId > 1) {
     currentPokemonId--;
@@ -259,6 +262,7 @@ function leftButton() {
   updateNavigationButtons();
 }
 
+// nach rechts navigieren
 function rightButton() {
   if (currentPokemonId < pokeLimit) {
     // pokeLimit ist die maximale Anzahl von Pokémon, die Sie anzeigen möchten
@@ -268,14 +272,10 @@ function rightButton() {
   updateNavigationButtons();
 }
 
+// beim ersten Pokemon wird der "rightButton" ausgeblendet und beim letzen Pokemon "leftButton" ausgeblendet
 function updateNavigationButtons() {
   const leftNavButton = document.getElementById("leftButton");
   const rightNavButton = document.getElementById("rightButton");
-
-  if (!leftNavButton || !rightNavButton) {
-    console.error("Navigationsbuttons nicht gefunden!");
-    return;
-  }
 
   if (currentPokemonId <= 1) {
     leftNavButton.style.display = "none";
@@ -290,6 +290,7 @@ function updateNavigationButtons() {
   }
 }
 
+// Zeigt den Bereich "About" an und blendet stats und ablilities aus
 function openAbout() {
   const aboutDetails = document.querySelector(".box-info-about");
   const statsDetails = document.querySelector(".box-info-stats-details");
@@ -300,6 +301,7 @@ function openAbout() {
   abilityDetails.classList.add("hidden");
 }
 
+// Zeigt den Bereich "stats" an und blendet about und ablilities aus
 function openChart() {
   const aboutDetails = document.querySelector(".box-info-about");
   const statsDetails = document.querySelector(".box-info-stats-details");
@@ -310,6 +312,7 @@ function openChart() {
   abilityDetails.classList.add("hidden");
 }
 
+// Zeigt den Bereich "ablilities" an und blendet about und stats aus
 function openAbilities() {
   const aboutDetails = document.querySelector(".box-info-about");
   const statsDetails = document.querySelector(".box-info-stats-details");
@@ -319,9 +322,5 @@ function openAbilities() {
   statsDetails.classList.add("hidden");
   abilityDetails.classList.remove("hidden");
 }
-
-
-
-
 
 initPokemon();
